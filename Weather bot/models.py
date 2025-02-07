dba61e4e4a3bb03c133225763b76f547230f9b34
ElenTurokamo <117432225+ElenTurokamo@users.noreply.github.com>
2025-02-07 22:47:54 +0500

    DeusBOT Alpha 0.4:
    - Добавлено больше информации о текущей погоде.
    - Обновлено оформление бота.
    - Создана система проверки погоды в городах, сохранённых пользователями.
    - Добавлены уведомления об изменении погоды в регионе.
    - Обновлено шифрование для защиты от утечек.

--- Weather bot/models.py
+++ Weather bot/models.py
@@ -1,11 +1,53 @@
-from sqlalchemy import Column, Integer, BigInteger, String
+from cryptography.fernet import Fernet
+import os
+from sqlalchemy import Column, Integer, BigInteger, String, Float, Boolean, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+
 Base = declarative_base()
+
+ENCRYPTION_KEY = os.getenv("ENCRYPTION_KEY")
+if not ENCRYPTION_KEY:
+    raise ValueError("Отсутствует ключ шифрования! Добавьте ENCRYPTION_KEY в .env")
+
+encryptor = Fernet(ENCRYPTION_KEY.encode())
+
+def encrypt_data(data):
+    return encryptor.encrypt(data.encode()).decode() if data else None
+
+def decrypt_data(encrypted_data):
+    return encryptor.decrypt(encrypted_data.encode()).decode() if encrypted_data else None
 
 class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(BigInteger, unique=True, nullable=False) 
-    username = Column(String(50), unique=True, nullable=True)
-    preferred_city = Column(String(100), nullable=True)
+    user_id = Column(BigInteger, unique=True, nullable=False)
+    username = Column(String(255), unique=True, nullable=True)
+    preferred_city = Column(String(255), nullable=True)
+    notifications_enabled = Column(Boolean, default=False, server_default='0', nullable=False)
+
+    def set_username(self, username):
+        self.username = encrypt_data(username)
+
+    def get_username(self):
+        return decrypt_data(self.username) if self.username else None
+
+    def set_preferred_city(self, city):
+        self.preferred_city = encrypt_data(city)
+
+    def get_preferred_city(self):
+        return decrypt_data(self.preferred_city) if self.preferred_city else None
+
+class CheckedCities(Base):
+    __tablename__ = 'checked_cities'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    city_name = Column(String(100), unique=True, nullable=False)
+    weather_info = Column(String(255), nullable=False)
+    temperature = Column(Float, nullable=False)
+    last_checked = Column(DateTime, default=datetime.utcnow)
+    
+    last_temperature = Column(Float, nullable=True)
+    last_wind_speed = Column(Float, nullable=True)
+    last_humidity = Column(Integer, nullable=True)
